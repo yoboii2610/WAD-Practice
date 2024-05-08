dab9c70925379a3cf32d815d2c1c473339908e78
Devansh0012 <devanshdubey0012@gmail.com>
2024-05-09 03:04:15 +0530

    updated node

--- Assignment-3/Part-A/app.js
+++ Assignment-3/Part-A/app.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const path = require('path');
-
+const express = require ('express');
 const app = express();
-const port = 3000; // Change the port if needed
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+app.use(express.static('public'));
+app.listen(3000,() => {
+    console.log('Application is started');
+})
